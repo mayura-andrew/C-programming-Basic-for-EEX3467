b4180fcb00a1bf17ed559b32a04d48acd784cef6
mayura-alahakoon <mayuraalahakoon@gmail.com>
2023-02-03 14:54:56 +0530

    init

---
+++ edX-C-programming/correctOrder.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    int i;
+
+    for (i = 0; i < 3; i++)
+    {
+        printf("I ");
+        printf("love ");
+        printf("programming!");
+    }
+
+    return (0);
+}
