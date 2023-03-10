34380160fb2f55580d6be0a17da45c35c9876939
mayura-alahakoon <mayuraalahakoon@gmail.com>
2023-03-10 20:48:57 +0530

    while

--- guessGameEdx.c
+++ guessGameEdx.c
@@ -51,3 +51,27 @@ int main(void){
 //     printf("Number of tries needed: \n %d", count);
 //     return 0;
 // }
+
+// given answer
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int secretNumber;
+//     scanf("%d",&secretNumber);
+//     int proposal = secretNumber + 1;
+//     int nbTries = 0;
+//     while (proposal != secretNumber)
+//     {
+//         scanf("%d",&proposal);
+//         if (proposal < secretNumber)
+//             printf("it is more\n");
+//         if (proposal > secretNumber)
+//             printf("it is less\n");
+//         nbTries = nbTries + 1;
+//     }
+//     printf("Number of tries needed:\n%d\n",nbTries);
+
+//     return 0;
+// }
