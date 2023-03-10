e286123b50e113a672de94d2771896cd12e7bce4
mayura-alahakoon <mayuraalahakoon@gmail.com>
2023-03-10 20:47:43 +0530

    while in C

---
+++ guessGameEdx.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main(void){
+
+    int guessValue;
+    int guessCount;
+    int userGuess;
+
+    scanf("%d", &guessValue);
+    while(userGuess != guessValue){
+
+        scanf("%d", &userGuess);
+
+        if (userGuess == guessValue){
+            guessCount += 1;
+            break;
+        } else if (userGuess > guessValue){
+             guessCount += 1;
+            printf("it is more\n");
+        } else {
+            printf("it is less\n");
+            guessCount += 1;
+        }
+       
+
+
+    }
+    printf("Number of tries needed %d:\n", guessCount);
+    
+    return 0;
+
+}
+
+// gpt answer 
+// #include <stdio.h>
+
+// int main() {
+//     int secretNumber, guess, count = 0;
+//     scanf("%d", &secretNumber);
+
+//     do {
+//         scanf("%d", &guess);
+//         count++;
+//         if (guess < secretNumber) {
+//             printf("it is more\n");
+//         } else if (guess > secretNumber) {
+//             printf("it is less\n");
+//         }
+//     } while (guess != secretNumber);
+
+//     printf("Number of tries needed: \n %d", count);
+//     return 0;
+// }
