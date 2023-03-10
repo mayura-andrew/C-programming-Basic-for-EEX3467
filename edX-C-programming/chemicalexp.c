6d4202fde910277d7b9a77b9382785392d40ae8c
mayura-alahakoon <mayuraalahakoon@gmail.com>
2023-03-10 20:53:59 +0530

    while loop in C

--- edX-C-programming/chemicalexp.c
+++ edX-C-programming/chemicalexp.c
@@ -18,4 +18,33 @@ int main() {
     }
 
     return 0;
+
+}
+
+
+//given answer 
+#include <stdio.h>
+int main()
+{
+    int tempMin, tempMax, temperature;
+    int tempIsValid = 1;
+    int end = 0;
+    scanf("%d%d", &tempMin, &tempMax);
+
+    while (!end && tempIsValid){
+        scanf("%d", &temperature);
+        end = (temperature == -999);
+
+        tempIsValid = (tempMin <= temperature && temperature <= tempMax);
+
+        if(!end){
+            if (tempIsValid){
+                printf("Nothing to report\n");
+            } else {
+                printf("Alert!\n");
+            }
+        }
+    }
+
+    return 0;
 }
