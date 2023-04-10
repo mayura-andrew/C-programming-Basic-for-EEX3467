aa799518c6cf1fc0f6a278320183be67c1279f0e
mayura-alahakoon <mayuraalahakoon@gmail.com>
2023-04-10 21:10:20 +0530

    c pointers

---
+++ edX-C-programming/unit4.1_Memory_and_Pointers/activity_use_pointers.c
@@ -0,0 +1,19 @@
+int main(void) {
+    int i;
+    int *iAdr = &i;
+ 
+    *iAdr = 10;
+ 
+    printf(" i = %d\n", i);
+    printf(" *iAdr = %d\n", *iAdr);
+ 
+    *iAdr = *iAdr - 2;
+    printf(" i = %d\n", i);
+    printf(" *iAdr = %d\n", *iAdr);
+ 
+    (*iAdr) += 1;
+    printf(" i = %d\n", i);
+    printf(" *iAdr = %d\n", *iAdr);
+ 
+    return 0;
+}
