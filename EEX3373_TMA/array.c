2b98d01a25a29a947441744e7507a08e7eca05af
mayura-alahakoon <mayuraalahakoon@gmail.com>
2023-04-24 01:36:18 +0530

    array - assembly

--- EEX3373_TMA/array.c
+++ EEX3373_TMA/array.c
@@ -10,8 +10,23 @@ void compute_averages(unsigned char Arr[x], float Avg[x/4]) {
     }
 }
 
+// Count the number of zeros in an array of x 8-bit binary numbers
+int count_zeros(unsigned char Arr[x]) {
+    int i, num_zeros = 0;
+    for (i = 0; i < x; i++) {
+        unsigned char byte = Arr[i];
+        int j;
+        for (j = 0; j < 8; j++) {
+            if ((byte & (1 << j)) == 0) {
+                num_zeros++;
+            }
+        }
+    }
+    return num_zeros;
+}
+
 int main() {
-    unsigned char Arr[x] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+    unsigned char Arr[x] = {0b00000000, 0b00000001, 0b00000010, 0b00000011, 0b00000100, 0b00000101, 0b00000110, 0b00000111, 0b00001000, 0b00001001, 0b00001010, 0b00001011, 0b00001100, 0b00001101, 0b00001110, 0b00001111, 0b00010000, 0b00010001, 0b00010010, 0b00010011, 0b00010100, 0b00010101, 0b00010110, 0b00010111, 0b00011000, 0b00011001, 0b00011010, 0b00011011, 0b00011100, 0b00011101, 0b00011110, 0b00011111};
     float Avg[x/4];
     int i, max_num = -1, max_avg_num = -1, num_zeros_Arr = 0, num_zeros_Avg = 0;
 
@@ -23,9 +38,6 @@ int main() {
         if (Arr[i] > max_num) {
             max_num = Arr[i];
         }
-        if (Arr[i] == 0) {
-            num_zeros_Arr++;
-        }
     }
 
     // Find the maximum averaged number in Avg[]
@@ -33,10 +45,11 @@ int main() {
         if (Avg[i] > max_avg_num) {
             max_avg_num = Avg[i];
         }
-        if (Avg[i] == 0) {
-            num_zeros_Avg++;
-        }
     }
+
+    // Count the number of zeros in Arr[x] and Avg[]
+    num_zeros_Arr = count_zeros(Arr);
+    num_zeros_Avg = count_zeros((unsigned char *) Avg);
 
     // Print the results
     printf("Maximum number in Arr[x]: %d\n", max_num);
